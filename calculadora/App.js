b4eb35b7ce70cda5ecde2ad4397e49b8989c58c0
gallojunior <gallojunior@gmail.com>
2026-02-25 09:47:39 -0300

    Criado o projeto, estrutura de pastas e arquivos

---
+++ calculadora/App.js
@@ -0,0 +1,28 @@
+import React, { useMemo, useState } from 'react';
+import { Pressable, StatusBar, StyleSheet, Text, View } from 'react-native';
+
+import CalcButton from "./components/CalcButton";
+import Display from "./components/Display";
+import { themes } from "./theme/token";
+import { createEngine } from "./utils/calcEngine";
+
+export default function App() {
+  const [mode, setMode] = useState("dark");
+  const theme = themes[mode];
+
+  return (
+    <View style={[styles.container, { backgroundColor : theme.bg }]}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
